19591e65ca28b9a8ecc141f6678eaa481e20e88a
Rafael Silva <119268545+rafaffels@users.noreply.github.com>
2025-05-30 12:36:01 -0300

    Update cal_num.py

--- cal_num.py
+++ cal_num.py
@@ -1,8 +1,8 @@
-num1 = int(input("1)Insira um numero: ")) #Aqui sera inserido um numero inteiro
-num2 = int(input("2)Insira um numero: ")) #Aqui sera inserido outro um numero inteiro
+num1 = int(input("1)Insira um numero: ")) #Aqui sera inserido um numero inteiro.
+num2 = int(input("2)Insira um numero: ")) #Aqui sera inserido outro um numero inteiro.
 
-cal = num1 != num2 #Usei o operador != que compara os valores para ver se o numero 1 é diferente do numero 2
+cal = num1 != num2 #Usei o operador != que compara os valores para ver se a entrada 1 é diferente da entrada 2.
 
-print(cal) # aqui sera imprimida a resposta se os valores são diferentes (true) ou se são iguais (False)
+print(cal) # aqui sera imprimida a resposta se os valores são diferentes (true) ou se são iguais (False).
     
     
